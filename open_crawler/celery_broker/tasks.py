bf64d663123e78e7529c0fd0224060331396e84d
kgrosjean <kevin.grosjean@coexya.eu>
2023-07-10 16:38:15 +0200

    improve(settings-update): Change settings update method

--- open_crawler/celery_broker/tasks.py
+++ open_crawler/celery_broker/tasks.py
@@ -3,6 +3,7 @@ from multiprocessing import Process
 
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
+from scrapy.utils.python import without_none_values
 
 from celery_broker.main import celery_app
 from open_crawler.crawler.spiders.menesr import MenesrSpider
@@ -12,9 +13,10 @@ logger = logging.getLogger(__name__)
 
 def crawl(url: str, depth: int, limit: int, headers: dict):
     crawler_settings = get_project_settings()
-    crawler_settings.set("DEPTH_LIMIT", depth)
-    crawler_settings.set("CLOSESPIDER_PAGECOUNT", limit)
-    crawler_settings.set("CUSTOM_HEADERS", headers or {})
+    custom_settings = without_none_values(
+        {"DEPTH_LIMIT": depth, "CLOSESPIDER_PAGECOUNT": limit, "CUSTOM_HEADERS": headers or {}}
+    )
+    crawler_settings.update(custom_settings)
     process = CrawlerProcess(settings=crawler_settings)
     process.crawl(MenesrSpider, url=url)
     process.start()
