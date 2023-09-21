bd9e20fd7de9caeb18cbf24b5ac86e1ffb2cc0d0
kgrosjean <kevin.grosjean@coexya.eu>
2023-09-21 12:00:00 +0200

    hotfix(html-middleware): Handle index.html

--- open_crawler/crawler/middlewares.py
+++ open_crawler/crawler/middlewares.py
@@ -49,9 +49,11 @@ class HtmlStorageMiddleware:
         base_file_path = (
             f"{os.environ['LOCAL_FILES_PATH']}/{spider.crawl_process.id}"
         )
-        file_name = response.url.split(f"{domain}")[-1] or "index.html"
+        file_name = response.url.split(f"{domain}")[-1]
         if not file_name.endswith(".html"):
             file_name = f"{file_name}.html"
+        if file_name == ".html":
+            file_name = "index.html"
         return Path(
             f"{base_file_path}/{os.environ['HTML_FOLDER_NAME']}/{file_name}"
         )
