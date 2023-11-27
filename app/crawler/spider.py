4d9d44f819606b0b9218216f1f95cd1321cf558b
Yilei Pan <pandalei97@gmail.com>
2023-11-27 09:04:51 +0100

    Playwright support

--- app/crawler/spider.py
+++ app/crawler/spider.py
@@ -1,5 +1,6 @@
 from urllib.parse import urlparse
 
+from scrapy import Request
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
@@ -9,6 +10,7 @@ from app.models.process import CrawlProcess
 class MenesrSpider(CrawlSpider):
     name = "menesr"
     rules = (Rule(),)
+    use_playwright = False
 
     def __init__(self, crawl_process: CrawlProcess, *a, **kw):
         parsed_url = urlparse(crawl_process.config.url)
@@ -17,7 +19,36 @@ class MenesrSpider(CrawlSpider):
         self.allowed_domains = [parsed_url.netloc]
         self.start_urls = [crawl_process.config.url]
         self.crawl_process = crawl_process
+        self.use_playwright = crawl_process.config.parameters.use_playwright
         super().__init__(*a, **kw)
+
+    def start_requests(self):
+        for url in self.start_urls:
+            if self.use_playwright:
+                yield Request(url, self.parse, meta={
+                    'depth': 0, # Set the initial depth to 0
+                    "playwright": True,
+                    "playwright_page_methods": [
+                        ("evaluate", 'window.scrollTo(0, document.body.scrollHeight)')
+                    ]
+                })
+            else:
+                yield Request(url, self.parse)
+
+
+    def parse(self, response, **kwargs):
+        # Crawl the links in the response page and continue to crawl the next page
+        links = LinkExtractor().extract_links(response)
+        for link in links:
+            if self.use_playwright:
+                yield Request(link.url, self.parse, meta={
+                    "playwright": True,
+                    "playwright_page_methods": [
+                        ("evaluate", 'window.scrollTo(0, document.body.scrollHeight)')
+                    ]
+                })
+            else:
+                yield Request(link.url)
 
 
 if __name__ == "__main__":
